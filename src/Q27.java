a8f1b7db16e430481984d7ba8108a5bace1249aa
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2021-12-08 18:51:04 +0530

    Remove Element

--- src/Q27.java
+++ src/Q27.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=nums.length;
+        int arr[]= new int[k];
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val)
+                k--;
+            else
+                arr[c++] = nums[i];
+            
+        }
+        for(int i =0;i<c;i++)
+        nums[i] = arr[i];
+        return k;
+    }
+}
