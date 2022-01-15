26ed49502e9f7643e8ed4b400e744b29de2af45b
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-15 23:38:58 +0530

    First Missing Positive

---
+++ src/Q41.java
@@ -0,0 +1,11 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        s=set(nums)
+        for i in range(1,len(s)+2):
+            if(len(s)==1 and nums[0]>0):
+                if(nums[0]>1):
+                    return 1
+                else:
+                    return nums[0]+1
+            if i not in s:
+                return i
