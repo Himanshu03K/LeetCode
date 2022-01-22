900b9d7ca244e9d57639d6ab8d04b1771da1a1d2
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-22 22:18:20 +0530

    Single Number II

---
+++ src/Q137.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+     
+        int n=nums.length;
+    int count[] = new int[32]; 
+
+    
+    for (int i = 0; i < 32; i++) 
+        for (int j = 0; j < n; j++) 
+            if ((nums[j] & (1 << i)) != 0) 
+                count[i] += 1; 
+
+   
+    int res = 0; 
+    for (int i = 0; i < 32; i++) 
+        res += (count[i] % 3) * (1 << i); 
+    return res; 
+    }
+}
