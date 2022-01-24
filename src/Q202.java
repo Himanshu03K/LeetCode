12c51149b7b815b741fcd0d915e6135ec3783206
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-24 23:31:04 +0530

    Happy Number

--- src/Q202.java
+++ src/Q202.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    static int sqr(int n){
+        int res=0;
+        while(n>0){
+            res+=(n%10)*(n%10);
+            n/=10;
+        }
+        return res;
+    }
+    public boolean isHappy(int n) {
+        
+        while(n>9){
+            n=sqr(n);
+        }
+        
+        return n==1||n==7 ;
+    }
+}
