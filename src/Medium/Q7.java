2b228d75e69955b3678d6ca94d74dd6814c8c20c
Himanshu Kushwaha <kushwaha0312@gmail.com>
2022-09-20 21:06:30 +0530

    Reverse Integer

--- src/Medium/Q7.java
+++ src/Medium/Q7.java
@@ -1,4 +1,4 @@
-/*
+package Medium;/*
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
@@ -46,7 +46,7 @@ class Q7 {
 //    public static void main(String []args){
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
-//        Q7 obj = new Q7();
+//        Medium.Q7 obj = new Medium.Q7();
 //        System.out.println(obj.reverse(n));
 //    }
 }
