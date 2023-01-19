9df211b8511fe7277908dd69eb4ae4e8312ae7d7
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2023-01-19 14:40:23 +0530

    Time: 1 ms (54.12%), Space: 41.9 MB (78.03%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,7 +13,13 @@
  *     }
  * }
  */
+
+public class DiaPair{
+    int ht =-1;
+    int d=0;
+}
 class Solution {
+    /*
     public int diameterOfBinaryTree(TreeNode root) {
         
         if(root == null)
@@ -34,4 +40,42 @@ class Solution {
         
         return Math.max(lh,rh)+1;
     }
+    */
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        return diameter(root).d;
+        
+    }
+    
+    public DiaPair diameter(TreeNode root){
+        
+        if(root == null)
+            return new DiaPair();
+        DiaPair ldp = diameter(root.left);
+        DiaPair rdp = diameter(root.right);
+        DiaPair sdp = new DiaPair();
+        
+        int sd= ldp.ht + rdp.ht +2;
+        sdp.d=Math.max(ldp.d,Math.max(rdp.d,sd));
+        sdp.ht = Math.max(ldp.ht, rdp.ht)+1;
+        return sdp;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
