d2d6a30d0669be6578e223811ff96420358611ec
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2023-07-05 16:56:08 +0530

    Time: 34 ms (99.20%), Space: 77 MB (94.97%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,71 +1,26 @@
 class Solution {
-   
-    public static class Node {
-        Node zero;
-        Node one;
-
-    }
-    public static void Insert(Node root,int val){
-Node curr= root;
-for (int i = 31; i >= 0; i--) {
-    if((val&(1<<i))==0){//check if particular bit is one or not 
-if(curr.zero!=null){
-    curr=curr.zero;
-}
-else{
-    Node nn= new Node();
-    curr.zero= nn;
-    curr= nn;
-}
-}
-else{
-    if(curr.one!=null){
-    curr=curr.one;
-}
-else{
-    Node nn= new Node();
-    curr.one= nn;
-    curr= nn;
-    }
-  }
-}
-}
-
-public static int getmaxXor(Node root,int val){
-    int num=0;
-    Node curr= root;
-    for (int i = 31; i >= 0; i--) {
-        int bit=(val&(1<<i));
-        if(bit==0){
-            if(curr.one!=null){
-                num+=(1<<i);
-                curr=curr.one;
-            }
-            else{
-                curr=curr.zero;
+    public int findMaximumXOR(int[] n) {
+        Set<Integer> set = new HashSet<>();
+        int mask = 0;
+        int max = 0;
+        for(int r : n) max = max >= r ? max : r;
+        int leftmost = 0;
+        while(max>0){
+            leftmost++;
+            max = max >>1;
+        }
+        for(int i = leftmost; i >=0; i--) {
+            mask |= 1<<i;
+            set.clear();
+            int tmp = max | (1<<i);
+            for(int s : n) {
+                if(set.contains((s & mask)^tmp)){
+                    max = tmp;
+                    break;
+                }
+                set.add(s & mask);
             }
         }
-        else{
-if(curr.zero!=null){
-     num+=(1<<i);
-     curr=curr.zero;
-}
-else{
-        curr=curr.one;
-}
-        }
-    }
-    return num;
-}
- public int findMaximumXOR(int[] nums) {
-         Node root= new Node();
-          for(int val:nums){
-        Insert(root ,val);
-    }
-    int xor=Integer.MIN_VALUE;
-    for(int val:nums){
-        xor=Math.max(xor,getmaxXor(root, val));
-    }
-return xor;
+        return max;
     }
 }
